ddd4805fbad97de0aff6b8c2293109984ad7ffda
Nathan Stevens <nathan.stevens@instras.com>
2023-08-30 08:06:15 -0400

    Initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main script for python challege -- PyBank 
+
+Created on Wed Aug 30 07:53:41 2023
+
+@author: Nathan Stevens
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main script for python challege -- PyPoll
+
+Created on Wed Aug 30 07:53:41 2023
+
+@author: ns96
+"""
+
